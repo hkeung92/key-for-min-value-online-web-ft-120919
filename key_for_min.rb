98559f81d5d8a98ae30792953d5260328efcd811
Ho Yin Keung <hykeung92@gmail.com>
2019-12-15 07:09:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.collect do |item, i|
+    if i[0] < i[1] && i[0] < i[2]
+      item
+    elsif i[1] < i[0] && i[1] < i[2]
+      item
+    else
+      item[3]
 end
